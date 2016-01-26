c2999c7ad3ba7bbcb61bdf1614324697f6752f9d
Boris <bdumontet@telecomdesign.fr>
2016-01-26 15:12:33 +0000

    test de la mort

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,8 +19,8 @@ $message = htmlspecialchars($_POST['message']);
 $address = htmlspecialchars($_POST['address']);
 
 // Create the email and send the message
-$to = 'contact@vitalbase.fr' ; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'bdumontet@telecomdesign.fr' ; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "FR - Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nCompany: $company\n\nMessage:\n$message";
 $headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
