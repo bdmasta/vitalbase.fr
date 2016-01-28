7ee39c0160e3b4ca482eae2200ab496c0b7d127c
Boris <bdumontet@telecomdesign.fr>
2016-01-28 09:56:57 +0000

    Fix contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,10 +20,36 @@ $address = htmlspecialchars($_POST['address']);
 
 // Create the email and send the message
 $to = 'bdumontet@telecomdesign.fr' ; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "FR - Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nCompany: $company\n\nMessage:\n$message";
-$headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$email_subject = "FR - Nouveau contact:  $name";
+$email_body = '
+  <html>
+    <body>
+      <p>Tu viens de recevoir un nouveau message via le site www.vitalbase.fr.</p>
+      <span><strong>Nom</strong></span>: '.$name.'<br>
+      <span><strong>Email</strong></span>: '.$email_address.'<br>
+      <span><strong>Entreprise</strong></span>: '.$company.'<br>
+      <span><strong>Message</strong></span>:
+      <p>'.$message.'</p>
+    </body>
+  </html>
+'
+;
+
+
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
+$headers .="Content-Transfer-Encoding: 8bit" . "\r\n";
+
+$headers .= "From: $email_address" . "\r\n";
+$headers .= "Reply-To: $email_address" . "\r\n";
+
+
+
+ // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+
+
+
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
